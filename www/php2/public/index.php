0f85db093f000b53cee4912baeeef9a9c9121b09
Lis1337 <il4991@yandex.ru>
2022-02-21 21:57:23 +0500

    bugfix

--- www/php2/public/index.php
+++ www/php2/public/index.php
@@ -7,16 +7,17 @@ require __DIR__ . '/autoload.php';
 
 $serverExplode = explode('/', $_SERVER['REQUEST_URI']);
 $ctrlName = $serverExplode[1];
+$methodName = $serverExplode[2];
+
+$asd = new \App\Controllers\Article();
 
 
 if (class_exists('\App\Controllers\\' . $ctrlName)) {
     $class = '\App\Controllers\\' . $ctrlName;
     $ctrlName = new $class;
 
-    if (isset($serverExplode[2])) {
-        $methodName = $serverExplode[2];
+    if (isset($serverExplode[2]) && method_exists($ctrlName, $methodName)) {
         $ctrlName->$methodName();
-
     } else {
         $ctrlName->read();
     }
