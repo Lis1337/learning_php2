c67b03db0100e90728ddf61c83c79ec6b8907677
Lis1337 <il4991@yandex.ru>
2022-02-21 22:07:02 +0500

    index method_exists

--- www/php2/public/index.php
+++ www/php2/public/index.php
@@ -7,7 +7,6 @@ require __DIR__ . '/autoload.php';
 
 $serverExplode = explode('/', $_SERVER['REQUEST_URI']);
 $ctrlName = $serverExplode[1];
-$methodName = $serverExplode[2];
 
 $asd = new \App\Controllers\Article();
 
@@ -16,11 +15,16 @@ if (class_exists('\App\Controllers\\' . $ctrlName)) {
     $class = '\App\Controllers\\' . $ctrlName;
     $ctrlName = new $class;
 
-    if (isset($serverExplode[2]) && method_exists($ctrlName, $methodName)) {
-        $ctrlName->$methodName();
+    if (isset($methodName)) {
+        $methodName = $serverExplode[2];
+
+        if (method_exists($ctrlName, $methodName)) {
+            $ctrlName->$methodName();
+        }
     } else {
         $ctrlName->read();
     }
+
 } else {
     die('error 404');
 }
